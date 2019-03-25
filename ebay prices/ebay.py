e177ca4d579d994864c18f2575b46ec1835014ab
Main Desktop <robrousejr@gmail.com>
2019-03-24 23:15:09 -0400

    Add more files

---
+++ ebay prices/ebay.py
@@ -0,0 +1,9 @@
+from script import *
+
+"""
+File meant for ebay functions, allows us to ask user for search query and conditions, 
+and build a list based off of this information
+
+eBay URL Parameters:
+http://www.helios825.org/url-parameters.php
+"""
