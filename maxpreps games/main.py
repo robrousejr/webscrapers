a672e5878b5ec231ee135de7bbe0d5e8b86600c3
Main Desktop <robrousejr@gmail.com>
2020-03-21 00:32:55 -0400

    Store win/loss and scores

--- maxpreps games/main.py
+++ maxpreps games/main.py
@@ -9,10 +9,25 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 # Contains HTML of team names played
 schoolsPlayedSoup = soup.select(".contest-type-indicator")
+scoresSoup = soup.select(".score")
 
-# Contains names of schools played
-schoolsPlayed = []
+schoolsPlayed = [] # Contains names of schools played
+winLoss = []
+tmpScores = [] # Contains temporary strings of 
 
+# Store names of schools played 
 for x in schoolsPlayedSoup:
     name = (x.text.split('(', 1)[0]).strip() # School name without whitespace
     schoolsPlayed.append(name)
+
+# Regex patterns
+winOrLossRegex = re.compile(r'\(\D\)') # Gets (W) or (L) at start
+scoreRegex = re.compile(r'[0-9]{1,3}') # Gets between 1-3 digits
+
+for x in scoresSoup:
+    match = winOrLossRegex.search(x.text).group().strip()
+    winOrLoss = match[1:2] # Gets 'W' or 'L'
+    match = scoreRegex.findall(x.text) # Holds list of this team's score and opponent's score
+    winLoss.append(winOrLoss)
+    tmpScores.append(match) 
+    print(winOrLoss + " " + match[0] + " - " + match[1])
