4f24e4e51a3c64dc81a2ce09cfed55826439e880
Main Desktop <robrousejr@gmail.com>
2020-03-21 15:15:54 -0400

    Extract season year

--- maxpreps games/main.py
+++ maxpreps games/main.py
@@ -11,9 +11,15 @@ url = "https://maxpreps.com/high-schools/" + schoolLink + "/basketball/schedule.
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
-# Contains HTML of team names played
-schoolsPlayedSoup = soup.select(".contest-type-indicator")
-scoresSoup = soup.select(".score")
+# Regex patterns
+winOrLossRegex = re.compile(r'\(\D\)') # Gets (W) or (L) at start
+scoreRegex = re.compile(r'[0-9]{1,3}') # Gets between 1-3 digits
+yearRegex = re.compile(r'\d{4}') # Gets first 4 digit number (year)
+
+schoolsPlayedSoup = soup.select(".contest-type-indicator") # Contains HTML of team names played
+scoresSoup = soup.select(".score") # Contains html of scores
+year = (soup.select("#ctl00_NavigationWithContentOverRelated_ContentOverRelated_PageHeaderUserControl_Team"))[0].text # In format "Varsity 2019-20"
+year = re.search(yearRegex, year).group().strip() # Year number without whitespace
 
 # Lists containing game information
 schoolsPlayed = [] # Contains names of schools played
@@ -24,10 +30,6 @@ scores = [] # Contains temporary strings of
 for x in schoolsPlayedSoup:
     name = (x.text.split('(', 1)[0]).strip() # School name without whitespace
     schoolsPlayed.append(name)
-
-# Regex patterns
-winOrLossRegex = re.compile(r'\(\D\)') # Gets (W) or (L) at start
-scoreRegex = re.compile(r'[0-9]{1,3}') # Gets between 1-3 digits
 
 # Fill lists of school names, win/loss, and scores
 for x in scoresSoup:
@@ -51,17 +53,17 @@ output.write("INSERT INTO game VALUES\n")
 thisSchoolNum = findIndexOfSchoolName(schoolName, databaseSchoolNumbers)
 
 """
-TODO: Output date
+TODO: Output date ("yyyy-mm-dd")
 TODO: Make functions and clean up code
 """
 
 # For every game, output into sql file
-for idx, game in enumerate(schoolsPlayed):
-    opponentSchoolNum = findIndexOfSchoolName(schoolsPlayed[idx], databaseSchoolNumbers)
+for iterNum, game in enumerate(schoolsPlayed):
+    opponentSchoolNum = findIndexOfSchoolName(schoolsPlayed[iterNum], databaseSchoolNumbers)
     opponentSchoolName = "NULL"
     if(opponentSchoolNum == -1):
         opponentSchoolNum = "NULL"
-        opponentSchoolName = "'" + schoolsPlayed[idx] + "'"
+        opponentSchoolName = "'" + schoolsPlayed[iterNum] + "'"
 
-    string = f"(NULL, {thisSchoolNum}, {opponentSchoolNum}, {scores[idx][0]}, {scores[idx][1]}, {opponentSchoolName}, NULL, 2019),\n"
+    string = f"(NULL, {thisSchoolNum}, {opponentSchoolNum}, {scores[iterNum][0]}, {scores[iterNum][1]}, {opponentSchoolName}, NULL, 2019),\n"
     output.write(string)
