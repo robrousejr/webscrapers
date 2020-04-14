988f5968c7771dacd306ea5e333b1bf0cbb39c84
Main Desktop <robrousejr@gmail.com>
2020-04-14 16:08:32 -0400

    Remove unneeded python file

--- maxpreps games/main.py
+++ maxpreps games/main.py
@@ -13,8 +13,6 @@ url = "https://www.maxpreps.com/high-schools/mckinley-trojans-(sebring,oh)/baske
 
 ########################################
 
-
-
 # Finds the index of the current school being worked on in databaseSchoolNumbers
 thisSchoolNum = findIndexOfSchoolName(schoolName, databaseSchoolNumbers)
 
